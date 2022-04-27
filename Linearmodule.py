9eb0502fad9c7bebaa806d026c7646a87f9966fa
stanislavnozdrin <stas.nozdrin@bk.ru>
2022-04-27 15:32:34 +0300

    Initial commit

---
+++ Linearmodule.py
@@ -0,0 +1,6 @@
+
+def PrintHello(word='world'):
+    print('Hello, {}'.format(word))
+
+if __name__ == '__main__':
+    PrintHello(word='Stanislav')
---
+++ main.py
@@ -0,0 +1,15 @@
+from Linearmodule import PrintHello
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print_hi('PyCharm')
+    PrintHello()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
